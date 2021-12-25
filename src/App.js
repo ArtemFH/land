0d7bf9e1aac4a542e536c5dd2cb9ccdb15fd6e93
ArtemFH <artembereza1@mail.ru>
2021-12-26 00:32:11 +0300

    test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import {routes} from "./routes/routes";
 import {useRoutes} from "react-router-dom";
 
 function App() {
+    const router = useRoutes(routes);
     return (
-        <>{useRoutes(routes)}</>
+        <>{router}</>
     );
 }
 
