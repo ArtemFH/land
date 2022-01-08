2f2eb8438af701e31846b743e744cb6fb5b13691
ArtemFH <artembereza1@mail.ru>
2022-01-09 04:45:05 +0700

    solved the problem with styles

--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -6,33 +6,19 @@ import {styled} from "@mui/system";
 function NavBar() {
     return (
         <StyledDiv>
-            <Box style={{
-                display: "flex",
-                justifyContent: "space-around",
-                height: "50px",
-                padding: "30px 115px"
-            }}>
-                <Link style={{textDecoration: "none"}} to={''}>
+            <Box style={styles.container}>
+                <Link style={styles.buttonStyleFirst} to={''}>
                     <img src={logo} alt={''}/>
                 </Link>
-                <Box style={{
-                    width: "410px",
-                    display: "flex",
-                    justifyContent: "space-between"
-                }}>
-                    <Link style={ButtonStyle} to={'stores'}>
+                <Box style={styles.boxStyle}>
+                    <Link style={styles.buttonStyleFirst} to={'stores'}>
                         <Button>Stores</Button>
                     </Link>
-                    <Link style={ButtonStyle} to={'help-support'}>
+                    <Link style={styles.buttonStyleFirst} to={'help-support'}>
                         <Button>Help & Support</Button>
                     </Link>
-                    <Link style={ButtonStyle} to={'download'}>
-                        <Button style={{
-                            height: "50px",
-                            width: "120px",
-                            backgroundColor: "#0F2336",
-                            color: "#FFFFFF"
-                        }}>Download</Button>
+                    <Link style={styles.buttonStyleLast} to={'download'}>
+                        <Button style={styles.buttonDownload}>Download</Button>
                     </Link>
                 </Box>
             </Box>
@@ -46,7 +32,25 @@ const StyledDiv = styled('div')({
     backgroundColor: "yellow",
 })
 
-const ButtonStyle = {
-    textDecoration: "none",
-    display: "grid"
+const styles = {
+    buttonStyleFirst: {
+        textDecoration: "none",
+        display: "grid"
+    }, buttonStyleLast: {
+        textDecoration: "line-through"
+    }, buttonDownload: {
+        height: "50px",
+        width: "120px",
+        backgroundColor: "#0F2336",
+        color: "#FFFFFF"
+    }, boxStyle: {
+        width: "410px",
+        display: "flex",
+        justifyContent: "space-between"
+    }, container: {
+        display: "flex",
+        justifyContent: "space-around",
+        height: "50px",
+        padding: "30px 115px"
+    }
 }
